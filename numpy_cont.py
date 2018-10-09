2ca056ed12acae6c5c9c23250c880594c5827bff
lwooliev <43651592+lwooliev@users.noreply.github.com>
2018-10-09 11:25:47 -0700

    Add files via upload

---
+++ numpy_cont.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+x = 1.0		#define a float
+y = 2.0		#define another float
+
+#exponents and logarithms
+print(np.exp(x))	#e^x
+print(np.log(x))	#ln x
+print(np.log10(x))	#log_10 x
+print(np.log2(x))	#log_2 x
+
+#min/max/misc
+print 
